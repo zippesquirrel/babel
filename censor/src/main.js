eaf58964b0ee765b7fe828cb2350ce34e210b0f3
zippesquirrel <maxkinsberg@gmail.com>
2026-04-28 13:39:47 -0600

    Advanced censorship with lang detection
    eld needed now, fs optional

--- censor/src/main.js
+++ censor/src/main.js
@@ -1,9 +1,26 @@
 import { env, pipeline } from '@huggingface/transformers';
+import { eld } from 'eld/large' // use .mjs extension for version <18
 import GUI from 'lil-gui'; 
+import fs from 'fs';
+
+// console.log(fs.readFile("snitch.txt"));
 
 env.backends.onnx.wasm.wasmPaths = 'https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/';
 
+
+let caught_bans = new Array()
 const classifier = await pipeline('sentiment-analysis');
+
+const banned_words = ['sigma', 'chingchong','yourmom','yomama','yourmama',
+  'yomom','cracker','frog','chinkylee','bokchoylee','adolf','hitler','nazi',
+  'swastika','gay','woke', "fart", "queef", "shart", "skidmark", "dingleberry", 
+  "turd", "poop", "peepee", "weewee", "weiner", "schlong", "dong",
+  "wang", "willy", "hooha", "vajayjay", "coochie", "cooch",
+  "punani", "snatch", "muff", "beaver", "box", "beef curtains","epstein","jeffrey",
+  "trump","coloniz","black", "choppleganger", "chud", "foid", "femoid", "chopped", 
+  "maximillianbobrossian", "smorganboard", "calvin klein", "sixseven", "67", "six7", 
+  "6seven", "six-seven","69", "rizzler", "niger", "palestine", "iran", "israel", "jew", 
+  "skibidi", "northkorea", "kimjon", "china","russia","ukraine", "isreal", "isreel", "yahu", "precum", "netanyahu"]
 
 // obscenity function
 
@@ -14,12 +31,12 @@ const matcher = new RegExpMatcher({
     ...englishRecommendedTransformers,
 })
 
-const params = { threshold: 0.5 }
+const params = { threshold: 0.99 }
 
 
 // lil gui
 const gui = new GUI();
-gui.add(params, "threshold", 0, 1, 0.01);
+gui.add(params, "threshold", 0.9, 1, 0.001);
 
 
 
@@ -31,12 +48,18 @@ let globalProfanityBool = new Boolean
 
 const inputElement = document.getElementById("input1")
 
+const banned_string = banned_words.join('\\b|\\b')
+    // console.log(banned_string)
+    const regex = RegExp(`\\b${banned_string}\\b`)
+    console.log(regex)
+
 function checkInput(string) {
   let profanityBool = new Boolean
-  if (matcher.hasMatch(string)) {
-    console.log('Profanity detected.');
+  if (matcher.hasMatch(string) || regex.test(string.toLowerCase()) || eld.detect(string).language !== "en") {
+    // console.log('Profanity detected.');
     profanityBool = true
     globalProfanityBool = true
+    caught_bans.push(string)
     // event.target.style.backgroundColor = "#ff0000";
     } 
     // no profanity
@@ -57,8 +80,12 @@ function displayAllowed(bool, id) {
 }
 
 
+
 inputElement.addEventListener("keydown", 
   async function (event) {
+    
+    // console.log("Regex:")
+    
     let allowed = new Boolean
     // old text for live listener/update
     // const value = event.target.value;
@@ -75,6 +102,13 @@ inputElement.addEventListener("keydown",
     const value = inputElement.value.trim();
     // return if empty
     if (!value) return;
+      console.log(eld.detect(value))
+      console.log(regex.test(value.toLowerCase()))
+      console.log((value.toLowerCase().replace(/\s/g,"")));
+      // console.log(banned_words.some(CheckWords()));
+      
+      // console.log("some check"+banned_words.some(CheckWords(value)));
+      
       // actual gizmo
       console.log(value);
 
@@ -99,12 +133,8 @@ inputElement.addEventListener("keydown",
       // console.log("id: " + event.target.id)
       document.getElementById("verdict").innerHTML=displayAllowed(allowed, event.target.id)
 
+      console.log(caught_bans)
+
     inputElement.value=""
     
 })
-
-
-// sentiment
-
-// console.log("start")
-
